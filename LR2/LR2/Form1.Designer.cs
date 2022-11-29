f36c8316373c10670800fa23d976e2cc95178822
Denis Yemets <denia0202@ukr.net>
2022-11-29 11:42:27 +0200

    Download on github

---
+++ LR2/LR2/Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿
+namespace LR2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelShopName = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelShopName
+            // 
+            this.labelShopName.AutoSize = true;
+            this.labelShopName.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelShopName.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.labelShopName.Location = new System.Drawing.Point(282, 41);
+            this.labelShopName.Name = "labelShopName";
+            this.labelShopName.Size = new System.Drawing.Size(0, 34);
+            this.labelShopName.TabIndex = 0;
+            // 
+            // listBox1
+            // 
+            this.listBox1.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.listBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.listBox1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 22;
+            this.listBox1.Location = new System.Drawing.Point(31, 105);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(731, 242);
+            this.listBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.button1.Font = new System.Drawing.Font("Times New Roman", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(31, 353);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(149, 26);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Дохід за місяць";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.button2.Font = new System.Drawing.Font("Times New Roman", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(31, 385);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(149, 26);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Дохід за рік";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.button3.Font = new System.Drawing.Font("Times New Roman", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.Location = new System.Drawing.Point(186, 353);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(202, 26);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Додати співробітника";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.button4.Font = new System.Drawing.Font("Times New Roman", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button4.Location = new System.Drawing.Point(186, 385);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(202, 26);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Видалити співробітника";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(578, 353);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(184, 38);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "Сума річного податку";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
+            this.comboBox1.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.comboBox1.Font = new System.Drawing.Font("Times New Roman", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.comboBox1.Location = new System.Drawing.Point(641, 28);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 23);
+            this.comboBox1.TabIndex = 8;
+            this.comboBox1.SelectionChangeCommitted += new System.EventHandler(this.comboBox1_SelectionChangeCommitted);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(637, 6);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(133, 19);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Список магазинів";
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Times New Roman", 10F);
+            this.button6.Location = new System.Drawing.Point(578, 70);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(184, 29);
+            this.button6.TabIndex = 10;
+            this.button6.Text = "Створити магазин";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(578, 406);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(184, 38);
+            this.button7.TabIndex = 11;
+            this.button7.Text = "Порівняти магазини";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 527);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.labelShopName);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
+            this.Padding = new System.Windows.Forms.Padding(10);
+            this.Text = "Автомобільний магазин";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.Label labelShopName;
+        public System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        public System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+    }
+}
+
---
+++ LR2/LR2/Form1.cs
@@ -0,0 +1,270 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace LR2
+{
+    public partial class Form1 : Form
+    {
+        public CarShop GCarShop;
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            InactiveButtons();
+            comboBox1.Items.Add("Mexico Motors");
+            comboBox1.Items.Add("Tokyo Auto");
+        }
+        private void comboBox1_SelectionChangeCommitted(object sender, EventArgs e)
+        {
+            switch (comboBox1.SelectedIndex)
+            {
+                case 0:
+                    {
+                        GCarShop = new CarShop(12, 115, "Mexico Motors", "м. Мехіко", 150);
+                        labelShopName.Text = GCarShop.Property_ShopName.Trim();
+                        ActiveButtons();
+                        ToString(GCarShop);
+                        break;
+                    }
+                case 1:
+                    {
+                        GCarShop = new CarShop(20,300, "Tokyo Auto", "м. Токіо",500);
+                        labelShopName.Text = GCarShop.Property_ShopName.Trim();
+                        ActiveButtons();
+                        ToString(GCarShop);
+                        break;
+                    }
+                default:
+                    break;
+            }
+        }
+        private void InactiveButtons()
+        {
+            button1.Enabled = false;
+            button2.Enabled = false;
+            button3.Enabled = false;
+            button4.Enabled = false;
+            button5.Enabled = false;
+        }
+        private void ActiveButtons()
+        {
+            button1.Enabled = true;
+            button2.Enabled = true;
+            button3.Enabled = true;
+            button4.Enabled = true;
+            button5.Enabled = true;
+        }
+        private void ToString(CarShop carShop)
+        {
+            listBox1.Items.Clear();
+            listBox1.Items.Add("Кількість відділів:  " + carShop.Property_NumberOfDepartments);
+            listBox1.Items.Add("Кількість працівників:  " + carShop.Property_NumberOfEmployers);
+            listBox1.Items.Add("Назва магазину:  " + carShop.Property_ShopName);
+            listBox1.Items.Add("Адреса магазину:  " + carShop.Property_StoreAddres);
+            listBox1.Items.Add("Середній дохід магазину на місяць:  " + carShop.Property_AvgIncome);
+            listBox1.Items.Add("Загальна заробітна плата співробітників:  " + carShop.Property_Salary);
+            listBox1.Items.Add("Загальні витрати на кіпівлю товарів:  " + carShop.Property_Expenses);
+            listBox1.Items.Add("Кількість найменувань товарів:  " + carShop.Property_ProductsQuantity);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(string.Format("Дохід за місяць: {0}$", (GCarShop.Property_AvgIncome.ToString("###,###"))),"Розрахунок");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(string.Format("Дохід за рік: {0}$", (GCarShop.Property_AvgIncome *12).ToString("###,###")), "Розрахунок");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(string.Format("Річний податок(17%): {0}$", ((GCarShop.Property_AvgIncome * 12)*0.17).ToString("###,###")), "Податок");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            ToString(++GCarShop);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            ToString(--GCarShop);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            ActiveButtons();
+            Form2 f2 = new Form2();
+            f2.Owner = this;
+            f2.ShowDialog();
+            GCarShop = Form2.carShop;
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            int i = GCarShop.CompareTo(new CarShop(12, 115, "Mexico Motors", "м. Мехіко", 150));
+
+            if (i == 0) MessageBox.Show(GCarShop.Property_ShopName + " приносить менші доходи.");
+            if (i == 1) MessageBox.Show(GCarShop.Property_ShopName + " приносить більші доходи.");
+            if (i == 2) MessageBox.Show("Магазини приносять однакові доходи.");
+        }
+    }
+
+    public class CarShop : IComparable
+    {
+        private uint NumberOfDepartments;       //Кількість відділів
+        private uint NumberOfEmployers;         //Кількість співробітників
+        private string ShopName = "";                //Назва магазину
+        private string StoreAddres;             //Адреса магазину
+        private uint AvgIncome;                 //Середній дохід магазину на місяць 
+        private uint Salary;                    //Загальна заробітна плата співробітників
+        private uint Expenses;                  //Загальні витрати на кіпівлю товарів
+        private uint ProductsQuantity;          //Кількість найменувань товарів
+
+
+        //Properties 
+        public uint Property_NumberOfDepartments { get { return NumberOfDepartments; } set { NumberOfDepartments = value; } }
+        public uint Property_NumberOfEmployers { get { return NumberOfEmployers; } set { NumberOfEmployers = value; } }
+        public string Property_ShopName { get { return ShopName; } set { ShopName = value; } }
+        public string Property_StoreAddres { get { return StoreAddres; } set { StoreAddres = value; } }
+        public uint Property_AvgIncome { get { return AvgIncome; } set { AvgIncome = value; } }
+        public uint Property_Salary { get { return Salary; } set { Salary = value; } }
+        public uint Property_Expenses { get { return Expenses; } set { Expenses = value; } }
+        public uint Property_ProductsQuantity { get { return ProductsQuantity; } set { ProductsQuantity = value; } }
+
+        public CarShop() {}
+
+        public CarShop(int _NumberOfDepartments, int _NumberOfEmployers, string _ShopName, string _StoreAddres,
+            int _ProductsQuantity)
+        {
+            NumberOfDepartments = (uint)_NumberOfDepartments;
+            NumberOfEmployers = (uint)_NumberOfEmployers;
+            ShopName = _ShopName;
+            StoreAddres = _StoreAddres;
+            ProductsQuantity = (uint)_ProductsQuantity;
+            Expenses = (uint) ExpensesSum(this);
+            Salary = (uint) SalarySum(this);
+            AvgIncome = (uint) AvgSum(this);
+        }
+
+        private int ExpensesSum(CarShop carShop)
+        {
+            int sum = 0;
+            //Індексатор
+            Products[] pr = Products.CreateProducts(carShop);
+            for (int j = 0; j < pr.Length; j++)
+            {
+                sum += pr[j].Property_Cost;
+            }
+            return sum;
+        }
+        private int SalarySum(CarShop carShop)
+        {
+            int sum = 0;
+            Employees[] emp = Employees.SetSalaryforEmployee(carShop);
+            for(int j = 0; j < emp.Length; j++)
+            {
+                sum += emp[j].Property_Salary;
+            }
+            return sum;
+        }
+        private int AvgSum(CarShop carShop)
+        {
+            return ((int)carShop.NumberOfDepartments*50000)-(int)carShop.Expenses - (int)carShop.Salary;
+        }
+
+        public int CompareTo(Object obj)
+        {
+            CarShop cs = (CarShop) obj;
+            if(this.Property_AvgIncome > cs.Property_AvgIncome) return 1;
+            if (this.Property_AvgIncome == cs.Property_AvgIncome) return 2;
+            else return 0;
+        }
+
+        public static CarShop operator ++ (CarShop carShop)
+        {
+            carShop.NumberOfEmployers = (uint)carShop.NumberOfEmployers + 1;
+            return carShop;
+        }
+
+        public static CarShop operator -- (CarShop carShop)
+        {
+            carShop.NumberOfEmployers = (uint)carShop.NumberOfEmployers - 1;
+            return carShop;
+        }
+    }
+
+    public class Products
+    {
+        private string name;
+        private int cost;
+        public int Property_Cost { get { return cost; } set { cost = value; } }
+        public Products() { }
+        public Products(string _name, int _cost) 
+        {
+            name = _name;
+            cost = _cost;
+        }
+
+        static Products[] products = new Products[1];
+
+        //Indexator
+        public Products this[int index]
+        {
+            get { return products[index]; }
+            set { products[index] = value; }
+        }
+
+        public static Products[] CreateProducts(CarShop cs)
+        {
+            //Random rnd = new Random();
+            products = new Products[(int)cs.Property_ProductsQuantity];
+            for (int i = 0; i < products.Length; i++)
+            {
+                products[i] = new Products("wheel", 800);
+            }
+            return products;
+        }
+    }
+    public class Employees
+    {
+        int salary;
+
+        public int Property_Salary { get { return salary; } set { salary = value; } }
+
+        static Employees[] employees = new Employees[1];
+        //Indexator
+        public Employees this[int index]
+        {
+            get { return employees[index]; }
+            set { employees[index] = value; }
+        }
+
+        Employees(int _salary)
+        {
+            this.salary = _salary;
+        }
+
+        public static Employees[] SetSalaryforEmployee(CarShop cs)
+        {
+            employees = new Employees[(int) cs.Property_NumberOfEmployers];
+            for (int i = 0; i < employees.Length; i++)
+            {
+                employees[i] = new Employees(800);
+            }
+            return employees;
+        }
+    }
+}
